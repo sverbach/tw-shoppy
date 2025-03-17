63fe40b902686c08556c8bb3ae8cf4772ff18ee9
Sven Erbach <sven.erbach@protonmail.com>
2025-03-17 07:20:32 +0100

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [svelte()],
+  integrations: [svelte(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
