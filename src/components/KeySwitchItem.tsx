e578f7b154c6393463fd07a17a13dc721967bda4
Sven Erbach <sven.erbach@protonmail.com>
2025-05-29 15:05:27 +0200

    make add-to-cart button float top left

--- src/components/KeySwitchItem.tsx
+++ src/components/KeySwitchItem.tsx
@@ -25,36 +25,41 @@ export function KeySwitchItem({ product }: Props) {
   }
 
   return (
-    <a
-      href={`/switches/${product!.handle}`}
+    <button
+      type="button"
       className="flex h-[150px] w-[100px] flex-col rounded-md p-2 font-bold backdrop-blur-sm"
       tabIndex={0}
       role="button"
     >
-      <ShopifyImage
-        classList="z-10 overflow-hidden object-cover flex-none hover:translate-y-2 transition-transform"
-        loading="eager"
-        image={product!.images.nodes[0]}
-        sizes={`100px`}
-      />
+      <a href={`/switches/${product!.handle}`}>
+        <ShopifyImage
+          classList="z-10 overflow-hidden object-cover flex-none hover:translate-y-2 transition-transform"
+          loading="eager"
+          image={product!.images.nodes[0]}
+          sizes={`100px`}
+        />
+      </a>
       <div className="ms-1 flex flex-1 flex-col gap-1">
-        <span className="max-h-[30px] overflow-hidden text-xs">{product!.title}</span>
+        <a href={`/switches/${product!.handle}`} className="max-h-[30px] overflow-hidden text-start text-xs">
+          {product!.title}
+        </a>
         <div className="flex gap-1">
           <div className={cn('h-2 w-2 self-center rounded-full', variant.availableForSale ? 'bg-green-700' : 'bg-red-700')}></div>
           <span className="text-xs">{price}</span>
-          <Button
-            variant="ghost"
-            disabled={!!cart?.id || isCartUpdating}
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              handleClickAddToCart(variant.id);
-            }}
-          >
-            <ShoppingCart />
-          </Button>
         </div>
       </div>
-    </a>
+      <Button
+        variant="ghost"
+        className="absolute top-0 left-0"
+        disabled={!cart?.id || isCartUpdating}
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          handleClickAddToCart(variant.id);
+        }}
+      >
+        <ShoppingCart />
+      </Button>
+    </button>
   );
 }
