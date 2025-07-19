3bf2c44b4e7e2537c7b001cedf866d1d98780253
Sven Erbach <sven.erbach@protonmail.com>
2025-07-19 19:48:55 +0200

    use client:idle

--- src/components/ShortcutBadge.tsx
+++ src/components/ShortcutBadge.tsx
@@ -2,22 +2,22 @@ import type { z } from 'zod';
 import { Badge } from './badge';
 import type { Shortcut } from '@/utils/shortcuts';
 import { getUserAgent } from '@/utils/user-agent';
+import { useEffect, useState } from 'react';
 
 export interface Props {
   shortcut: z.infer<typeof Shortcut>;
 }
 export function ShortcutBadge({ shortcut }: Props) {
-  const userAgent = getUserAgent();
-  const letterDisplay = shortcut.key.toUpperCase();
-  const altKeyDisplay = shortcut.altKey ? 'ALT' : undefined;
-  const metaKeyDisplay = shortcut.metaKey ? (userAgent === 'mac' ? '⌘' : 'CTRL') : undefined;
+  const [keysDisplay, setKeysDisplay] = useState('');
+  useEffect(() => {
+    const userAgent = getUserAgent();
+    const letterDisplay = shortcut.key.toUpperCase();
+    const altKeyDisplay = shortcut.altKey ? 'ALT' : undefined;
+    const metaKeyDisplay = shortcut.metaKey ? (userAgent === 'mac' ? '⌘' : 'CTRL') : undefined;
 
-  const keys = [metaKeyDisplay, altKeyDisplay, letterDisplay].filter((key) => !!key);
-  const keysDisplay = keys.join('+');
+    const keys = [metaKeyDisplay, altKeyDisplay, letterDisplay].filter((key) => !!key);
+    setKeysDisplay(keys.join('+'));
+  }, []);
 
-  return (
-    <>
-      <Badge variant="outline">{keysDisplay}</Badge>
-    </>
-  );
+  return <>{keysDisplay === '' ? <div></div> : <Badge variant="outline">{keysDisplay}</Badge>}</>;
 }
